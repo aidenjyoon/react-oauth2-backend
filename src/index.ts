3c54863fb4a630320a46aef48a2c2e1a480a30d4
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-05-27 16:03:51 -0700

    implemented index.ts to successfully be able to auth
    using passport and passport-google-oauth20, able to successfully get user auth

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import session from "express-session";
+import passport from "passport";
+var GoogleStrategy = require("passport-google-oauth20");
+// import GoogleStrategy from "passport-google-oauth20"
 
 // load variables
 dotenv.config();
@@ -22,6 +25,54 @@ app.use(
     resave: true,
     saveUninitialized: true,
   })
+);
+
+// taking entire user object we get from the authentication method
+// and storing them into sessions
+passport.serializeUser((user: any, done: any) => {
+  return done(null, user);
+});
+
+// taking entire user object from the session and attaching it to the req.user object
+// (Bad. we should only store user ID in better apps)
+passport.deserializeUser((user: any, done: any) => {
+  return done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "712861010049-flgco3acveii0e8adca2knlk8htf5qi4.apps.googleusercontent.com",
+      clientSecret: "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",
+      callbackURL: "/auth/google/callback",
+    },
+    function (accessToken: any, refreshToken: any, profile: any, cb: any) {
+      // Called on Successful Authentication!
+      // Insert into Database
+      console.log(profile);
+      cb(null, profile);
+    }
+  )
+);
+
+// 1.
+// get request that will authenticate user based on the configuations above (GoogleStrategy we've implemented)
+// then calls the function on a successful authentication
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+// 2.
+// runs callback after to redirect to home
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect to home
+    res.redirect("/");
+  }
 );
 
 app.get("/", (req, res) => {
